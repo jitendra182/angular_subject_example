76fad0891ded8832e4613c8fc9fc58a09fd5a679
jitendra182 <jitendranayak182@gmail.com>
2021-03-09 19:12:08 +0530

    3 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
+import { Subscription } from "rxjs";
+import { MessageService } from "./message.service";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  constructor(public msgService: MessageService) {}
+  subscription: Subscription;
+  msg: string;
+  ngOnInit() {
+    this.subscription = this.msgService.message.subscribe(msg => {
+      this.msg = msg;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { MessageService } from './message.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [MessageService]
 })
 export class AppModule { }
